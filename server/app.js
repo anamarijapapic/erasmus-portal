f44c5150582567c5a5a08edb5168ac9049e21e7b
imihanovic <ivanamihanovic00@gmail.com>
2025-01-05 17:39:29 +0100

    Typo fixed (const)

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
-var subjectAreasRouter = require('./routes/subjectAreas');
-var studyProgrammesRouter = require('./routes/studyProgrammes');
+const subjectAreasRouter = require('./routes/subjectAreas');
+const studyProgrammesRouter = require('./routes/studyProgrammes');
 const departmentRouter = require('./routes/department');
 const institutionRouter = require('./routes/institutions');
 
 const { connectDB } = require('./config/database');
 
-var app = express();
+const app = express();
 connectDB();
 
 // view engine setup
