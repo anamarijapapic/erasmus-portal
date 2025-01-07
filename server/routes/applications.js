58e05addaa329e5451821c7628cf3a61f2867103
Tea27 <basic.tea2327@gmail.com>
2025-01-07 16:09:28 +0100

    Added file management on application using Multer and GridFS

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -1,22 +1,27 @@
 const express = require('express');
+var multer = require('multer');
 const router = express.Router();
-
 const {
   getAllApplications,
   getApplication,
   createApplication,
   updateApplication,
   deleteApplication,
+  downloadFile,
+  deleteFile,
+  uploadFile,
 } = require('../controllers/applicationController.js');
 
 router.get('/', getAllApplications);
 
 router.get('/:id', getApplication);
-
-router.post('/', createApplication);
-
+router.post('/', multer().array('files', 5), createApplication);
 router.patch('/:id', updateApplication);
 
 router.delete('/:id', deleteApplication);
+
+router.get('/downloadFile/:id', downloadFile);
+router.delete('/deleteFile/:id', deleteFile);
+router.post('/uploadFile/:id', multer().single('file'), uploadFile);
 
 module.exports = router;
